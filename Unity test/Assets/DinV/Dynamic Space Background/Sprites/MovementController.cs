e5cdcc483f97be453dc7adbca3db6b9b2becf15b
YOSHIZIN0 <39403749+YOSHIZIN0@users.noreply.github.com>
2020-11-18 16:16:49 +0900

    コントローラー

---
+++ Unity test/Assets/DinV/Dynamic Space Background/Sprites/MovementController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kukanidou : MonoBehaviour
+{
+	public bool CanMove = true;
+	public bool CanMoveForward = true;
+	public bool CanMoveBack = true;
+	public bool CanMoveLeft = true;
+	public bool CanMoveRight = true;
+	public bool CanMoveUp = true;
+	public bool CanMoveDown = true;
+	public bool CanRotateYaw = true;
+	public bool CanRotatePitch = true;
+	public bool CanRotateRoll = true;
+
+	public float MovementSpeed = 100f;
+	public float RotationSpeed = 100f;
+
+	private bool canTranslate;
+	private bool canRotate;
+	// Start is called before the first frame update
+	void Start()
+    {
+		canTranslate = CanRotateYaw || CanRotatePitch || CanRotateRoll;
+		canRotate = CanMoveForward || CanMoveBack || CanMoveRight || CanMoveLeft || CanMoveUp || CanMoveDown;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	void FixedUpdate()
+	{
+		if (CanMove)
+		{
+			UpdatePosition();
+		}
+	}
+
+	void UpdatePosition()
+	{
+		// Rotation
+		if (canRotate)
+		{
+			Quaternion AddRot = Quaternion.identity;
+			float yaw = 0;
+			float pitch = 0;
+			float roll = 0;
+			if (CanRotateYaw)
+			{
+				yaw = Input.GetAxis("Yaw") * (Time.fixedDeltaTime * RotationSpeed);
+			}
+			if (CanRotatePitch)
+			{
+				pitch = Input.GetAxis("Pitch") * (Time.fixedDeltaTime * RotationSpeed);
+			}
+			if (CanRotateRoll)
+			{
+				roll = Input.GetAxis("Roll") * (Time.fixedDeltaTime * RotationSpeed);
+			}
+			AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
+			GetComponent<Rigidbody>().rotation *= AddRot;
+		}
+
+		// Translation
+		if (canTranslate)
+		{
+			// Check key input
+			int[] input = new int[6]; // Forward, Back, Left, Right, Up, Down
+			if (CanMoveForward && Input.GetKey(KeyCode.U))
+			{
+				input[0] = 1;
+			}
+			else if (CanMoveBack && Input.GetKey(KeyCode.J))
+			{
+				input[1] = 1;
+			}
+			if (CanMoveLeft && Input.GetKey(KeyCode.H))
+			{
+				input[2] = 1;
+			}
+			else if (CanMoveRight && Input.GetKey(KeyCode.K))
+			{
+				input[3] = 1;
+			}
+			if (CanMoveUp && Input.GetKey(KeyCode.Y))
+			{
+				input[4] = 1;
+			}
+			else if (CanMoveDown && Input.GetKey(KeyCode.I))
+			{
+				input[5] = 1;
+			}
+			int numInput = 0;
+			for (int i = 0; i < 6; i++)
+			{
+				numInput += input[i];
+			}
+			// Add velocity to the gameobject
+			float curSpeed = numInput > 0 ? MovementSpeed : 0;
+			Vector3 AddPos = input[0] * Vector3.forward + input[2] * Vector3.left + input[4] * Vector3.up
+				+ input[1] * Vector3.back + input[3] * Vector3.right + input[5] * Vector3.down;
+			AddPos = GetComponent<Rigidbody>().rotation * AddPos;
+			GetComponent<Rigidbody>().velocity = AddPos * (Time.fixedDeltaTime * curSpeed);
+		}
+	}
+}
---
+++ controler/Assets/Flying.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying : MonoBehaviour
+{
+    public float MovementSpeed = 100f;          // 通常時の進行速度
+    public float MaxMovementSpeed = 1000f;      // ブースト時の進行速度
+    public float RotationSpeed = 100f;          // 回転速度
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
+
+        Quaternion AddRot = Quaternion.identity;
+        float yaw = 0;
+        float pitch = 0;
+        float roll = 0;
+
+        // 縦回転
+        yaw = Input.GetAxisRaw("Horizontal") * (Time.fixedDeltaTime * RotationSpeed);
+
+        // 横回転
+        pitch = Input.GetAxisRaw("Vertical") * (Time.fixedDeltaTime * RotationSpeed);
+
+        // 軸回転
+        if (Input.GetAxisRaw("roll_R") < 0.8)
+        {
+            roll = Input.GetAxisRaw("roll_R") * (Time.fixedDeltaTime * RotationSpeed);
+        }
+        else if (Input.GetAxisRaw("roll_L") < 0.8)
+        {
+            roll = -Input.GetAxisRaw("roll_L") * (Time.fixedDeltaTime * RotationSpeed);
+        }
+
+        AddRot.eulerAngles = new Vector3(-pitch, yaw, -roll);
+        GetComponent<Rigidbody>().rotation *= AddRot;
+
+        Vector3 AddPos = Vector3.zero;
+
+        // 前進
+        if (Input.GetAxisRaw("Mouse Y") < -0.1)
+        {
+            AddPos = Vector3.forward;
+        }
+        // 後進
+        else if (Input.GetAxisRaw("Mouse Y") > 0.1)
+        {
+            AddPos = Vector3.back;
+        }
+
+        AddPos = GetComponent<Rigidbody>().rotation * AddPos;
+
+        // ブースト
+        if (Input.GetButton("Boost"))
+        {
+            GetComponent<Rigidbody>().velocity = AddPos * (Time.fixedDeltaTime * MaxMovementSpeed);
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = AddPos * (Time.fixedDeltaTime * MovementSpeed);
+        }
+    }
+}
