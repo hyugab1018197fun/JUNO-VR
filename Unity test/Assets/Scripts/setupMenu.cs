7dbf6ee58a1eeab918df8fe4aee3358b0d985c80
Mika <67038474+Mika777v@users.noreply.github.com>
2020-12-02 17:24:36 +0900

    VRモードの追加

---
+++ Unity test/Assets/Scripts/setupMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class setupMenu : MonoBehaviour
+{
+//　画面UI
+	[SerializeField]
+	private GameObject moveWindow;
+    [SerializeField]
+    private GameObject menuWindow;
+	
+ 
+	void Update () {
+ 
+		//　スペースキーを押したら画面UIのオン・オフ
+		if (Input.GetKeyDown ("space")) {
+			menuWindow.SetActive (!menuWindow.activeSelf);
+ 			moveWindow.SetActive (!moveWindow.activeSelf);
+
+
+		}
+	}
+}
